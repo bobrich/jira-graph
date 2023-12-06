9cdc37f4464ad4ed77e4220e3af69abf45824ac8
Bob Rich <rrich@Bobs-Mac-mini.local>
2023-12-06 15:53:04 -0500

    moved page.tsx to app/ and created issue.js

--- pages/api/issue.js
+++ pages/api/issue.js
@@ -3,20 +3,27 @@ export default async function handler(req, res) {
     const { issueKey } = req.query;
   
     try {
-      const response = await fetch(`https://bobrich.net/rest/api/3/issue/${issueKey}`, { 
+      const response = await fetch(`https://bobrich.atlassian.net/rest/api/3/issue/${issueKey}`, {
         method: 'GET',
         headers: {
           'Authorization': 'Basic ' + btoa(`${process.env.JIRA_EMAIL}:${process.env.JIRA_API_TOKEN}`),
           'Accept': 'application/json'
         }
       });
+  
       if (!response.ok) {
-        throw new Error('Issue not found');
+        console.error('Jira API response not OK:', response.status, response.statusText);
+        throw new Error(`Jira API error: Status ${response.status}, Status Text: ${response.statusText}`);
       }
+  
       const data = await response.json();
       res.status(200).json(data);
     } catch (err) {
-      res.status(500).json({ error: err.message });
+      console.error('Error occurred in /api/issue:', err.message, 'Error type:', err.constructor.name);
+      res.status(500).json({ 
+        error: `An error occurred: ${err.message}`, 
+        type: err.constructor.name 
+      });
     }
   }
   
